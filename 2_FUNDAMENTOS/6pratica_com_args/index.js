02ffbab98a4c48198ce2817c75243d2e7f5c3574
wallace <wallace_wolv@hotmail.com>
2023-12-18 16:39:55 -0300

    feat: (Fundamentos) - Aula 6 Algo prático com argumentos

---
+++ 2_FUNDAMENTOS/6pratica_com_args/index.js
@@ -0,0 +1,11 @@
+// externo
+const minimist = require('minimist');
+const args = minimist(process.argv.slice(2));
+
+// interno
+const soma = require('./soma').soma;
+
+const a = parseInt(args["a"]);
+const b = parseInt(args["b"]);
+
+soma(a,b)
