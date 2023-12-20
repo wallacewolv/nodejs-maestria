0d4dc9dfc54ea2aa416183446d13eb60358a182d
wallace <wallace_wolv@hotmail.com>
2023-12-19 22:58:06 -0300

    feat: (Pojeto 1: Accounts) - Setup inicial

---
+++ 5_ACCOUNTS/index.js
@@ -0,0 +1,9 @@
+// modulos externos
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+
+// modulos internos
+const fs = require('fs');
+
+console.log('Iniciamos o Accounts');
+
