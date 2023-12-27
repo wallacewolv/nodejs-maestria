c2d7a78195e526690c30bb00e193bea53d95717e
wallace <wallace_wolv@hotmail.com>
2023-12-27 19:24:31 -0300

    feat: (Sequelize com Node.js) - Aula 12 Excluindo registro relacionado

---
+++ 9_SEQUELIZE/12_remover_relacionados/index.js
@@ -0,0 +1,117 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const conn = require('./db/connection');
+
+const User = require('./models/User');
+const Address = require('./models/Address');
+
+const app = express();
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+
+app.use(express.json());
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.get('/users/create', (req, res) => {
+  res.render('adduser');
+});
+
+app.post('/users/create', async (req, res) => {
+  const name = req.body.name;
+  const occupation = req.body.occupation;
+  const newsletter = req.body.newsletter === 'on';
+
+  await User.create({ name, occupation, newsletter });
+
+  res.redirect('/');
+});
+
+app.get('/users/:id', async (req, res) => {
+  const id = req.params.id;
+
+  const user = await User.findOne({ raw: true, where: { id: id } });
+
+  res.render('userview', { user });
+});
+
+app.get('/users/edit/:id', async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const user = await User.findOne({ include: Address, where: { id: id } });
+
+    res.render('useredit', { user: user.get({ plain: true }) });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post('/users/delete/:id', async (req, res) => {
+  const id = req.params.id;
+
+  await User.destroy({ where: { id: id } });
+
+  res.redirect('/');
+});
+
+app.post('/users/update', async (req, res) => {
+  const { id, name, occupation } = req.body;
+  const newsletter = req.body.newsletter === 'on';
+
+  const userData = { id, name, occupation, newsletter };
+
+  await User.update(userData, { where: { id: id } });
+
+  res.redirect('/');
+});
+
+app.get('/', async (req, res) => {
+  const users = await User.findAll({ raw: true });
+
+  res.render('home', { users });
+});
+
+app.post('/address/create', async (req, res) => {
+  const { UserId, street, number, city } = req.body;
+  const address = {
+    UserId,
+    street,
+    number,
+    city,
+  };
+
+  await Address.create(address);
+
+  console.log(address);
+
+  res.redirect(`/users/edit/${UserId}`);
+});
+
+app.post('/address/delete', async (req, res) => {
+  const UserId = req.body.UserId;
+  const id = req.body.id;
+
+  await Address.destroy({ where: { id: id } });
+
+  res.redirect(`/users/edit/${UserId}`);
+});
+
+conn
+  .sync()
+  // .sync({ force: true }) // Força o DROP DATA TABLE para reiniciar as tabelas
+  .then(() => {
+    console.log('Conectamos com sucesso com o Sequelize!');
+
+    app.listen(3000, () => {
+      console.log('App funcionando!');
+    });
+  })
+  .catch((err) => console.log(err));
