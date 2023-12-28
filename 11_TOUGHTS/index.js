09b3e721f3c73a6ac8718a537f93a1dbcf1895f0
wallace <wallace_wolv@hotmail.com>
2023-12-28 16:35:21 -0300

    feat: (TOUGHTS) - Aula 3 Finalizando estrutura

--- 11_TOUGHTS/index.js
+++ 11_TOUGHTS/index.js
@@ -8,6 +8,54 @@ const app = express();
 
 const conn = require('./db/connection');
 
+// template engine
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+// receber resposta do body
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+//session middleware
+app.use(
+  session({
+    name: 'session',
+    secret: 'nosso_secret',
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function () {},
+      path: require('path').join(require('os').tmpdir(), 'sessions'),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 3600000,
+      expires: new Date(Date.now() + 3600000),
+      httpOnly: true,
+    },
+  }),
+);
+
+// flash messages
+app.use(flash());
+
+// public path
+app.use(express.static('public'));
+
+// set session to res
+app.use((req, res, next) => {
+  if (req.session.userid) {
+    res.locals.session = req.session;
+  }
+
+  next();
+});
+
 conn
   .sync()
   .then(() => {
