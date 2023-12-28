dae13aff186d4d73c17d533105e2d54c2649f446
wallace <wallace_wolv@hotmail.com>
2023-12-28 17:08:49 -0300

    feat: (TOUGHTS) - Aula 5 Criando a home do projeto

--- 11_TOUGHTS/index.js
+++ 11_TOUGHTS/index.js
@@ -12,6 +12,12 @@ const conn = require('./db/connection');
 const Tought = require('./models/Tought');
 const User = require('./models/User');
 
+// Import Routes
+const toughtsRoutes = require('./routes/toughtsRoutes');
+
+// Import Controller
+const ToughtController = require('./controllers/ToughtController');
+
 // template engine
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
@@ -59,6 +65,11 @@ app.use((req, res, next) => {
 
   next();
 });
+
+// Routes
+app.use('/toughts', toughtsRoutes);
+
+app.use('/', ToughtController.showToughts);
 
 conn
   .sync()
