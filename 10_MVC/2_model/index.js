c3b76ebb3bda464f66aee0e4053f977ee9bfc26a
wallace <wallace_wolv@hotmail.com>
2023-12-28 11:59:28 -0300

    feat: (MVC) - Aula 2 Criando o nosso Model

---
+++ 10_MVC/2_model/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+const conn = require('./db/connection');
+
+const Task = require('./models/Task');
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+conn
+  .sync()
+  .then(() => {
+    app.listen(3000, () => {
+      console.log('App funcionando!');
+    });
+  })
+  .catch((err) => console.log(err));
