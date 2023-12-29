9788aeeb0fa3950993b7b7190f330e56043020a2
wallace <wallace_wolv@hotmail.com>
2023-12-29 13:36:06 -0300

    feat: (TOUGHTS) - Aula 15 Middleware de verificação de autenticação

--- 11_TOUGHTS/controllers/ToughtController.js
+++ 11_TOUGHTS/controllers/ToughtController.js
@@ -2,6 +2,10 @@ const Tought = require('../models/Tought');
 const User = require('../models/User');
 
 module.exports = class ToughtController {
+  static async dashboard(req, res) {
+    res.render('toughts/dashboard');
+  }
+ 
   static async showToughts(req, res) {
     res.render('toughts/home');
   }
--- 11_TOUGHTS/routes/toughtsRoutes.js
+++ 11_TOUGHTS/routes/toughtsRoutes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const ToughtController = require('../controllers/ToughtController');
 
+// helper
+const checkAuth = require('../helpers/auth').checkAuth;
+
+router.get('/dashboard', checkAuth, ToughtController.dashboard);
 router.get('/', ToughtController.showToughts);
 
 module.exports = router;
