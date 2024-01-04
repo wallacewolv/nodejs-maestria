abe04c91f2307b962bdaaea96463c126a388978d
wallace <wallace_wolv@hotmail.com>
2024-01-04 19:34:28 -0300

    feat: (GET A PET) - Aula 4 Validações de usuários

--- 15_GETAPET/backend/controllers/UserController.js
+++ 15_GETAPET/backend/controllers/UserController.js
@@ -2,6 +2,56 @@ const User = require('../models/User');
 
 module.exports = class UserController {
   static async register(req, res) {
-    res.json('Olá Get A Pet!');
+    const { name, email, phone, password, confirmpassword } = req.body;
+
+    // validations
+    if (!name) {
+      res.status(422).json({ message: 'O nome é obrigatório' });
+      return;
+    }
+
+    if (!email) {
+      res.status(422).json({ message: 'O e-mail é obrigatório' });
+      return;
+    }
+
+    if (!phone) {
+      res.status(422).json({ message: 'O telefone é obrigatório' });
+      return;
+    }
+
+    if (!password) {
+      res.status(422).json({ message: 'A senha é obrigatória' });
+      return;
+    }
+
+    if (!confirmpassword) {
+      res.status(422).json({ message: 'A confirmação da senha é obrigatória' });
+      return;
+    }
+
+    if (password !== confirmpassword) {
+      res.status(422).json({ message: 'A senha e a confirmação de senha precisam ser iguais!' });
+      return;
+    }
+
+    // check if user exists 
+    const userExists = await User.findOne({ email: email });
+
+    if (userExists) {
+      res.status(422).json({ message: 'Por favor, utilize outro e-mail!' });
+      return;
+    }
+
+    // check email is valid
+    const check = /\S+@\S+\.\S+/;
+    const emailValid = check.test(email);
+
+    if (!emailValid) {
+      res.status(422).json({ message: 'Por favor, insira um e-mail válido!' });
+      return;
+    }
+
+    console.log({ name, email, phone, password, confirmpassword });
   };
 }
