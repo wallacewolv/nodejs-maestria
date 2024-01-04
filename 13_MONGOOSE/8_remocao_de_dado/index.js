003171e6fe0cb15dfaa3f1b7048ddc911bdcd9e9
wallace <wallace_wolv@hotmail.com>
2024-01-04 15:58:51 -0300

    feat: (MONGOOSE) - Aula 8 Remoção de dados

---
+++ 13_MONGOOSE/8_remocao_de_dado/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+const conn = require('./db/connection');
+
+const productsRoutes = require('./routes/productsRouters');
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+// read body
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+);
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.use('/products', productsRoutes);
+
+app.listen(3000, () => {
+  console.log('App funcionando!');
+});
