42463d055d4afccc12933f77e663d6706cf406ac
wallace <wallace_wolv@hotmail.com>
2024-01-05 19:19:39 -0300

    feat: (GET A PET) - Aula 32 Criando página e form de registro

--- 15_GETAPET/frontend/src/components/pages/Auth/Register.js
+++ 15_GETAPET/frontend/src/components/pages/Auth/Register.js
@@ -1,7 +1,52 @@
+import Input from "../../form/Input";
+
 function Register() {
+
+  function handleChange() {
+
+  }
+
   return (
     <section>
-      <h1>Register</h1>
+      <h1>Registrar</h1>
+      <form>
+        <Input
+          text="Nome:"
+          type="text"
+          name="name"
+          placeholder="Digite o seu nome"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Telefone:"
+          type="text"
+          name="phone"
+          placeholder="Digite o seu telefone"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="E-mail:"
+          type="email"
+          name="email"
+          placeholder="Digite o seu e-mail"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Senha:"
+          type="password"
+          name="password"
+          placeholder="Digite a sua senha"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Confirmação de senha:"
+          type="password"
+          name="confirmpassword"
+          placeholder="Confirme a sua senha"
+          handleOnChange={handleChange}
+        />
+        <Input type="submit" value="Cadastrar" />
+      </form>
     </section>
   );
 }
