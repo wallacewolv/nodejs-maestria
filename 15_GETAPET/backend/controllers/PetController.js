0a740150520ba7b3feda6339268252f8f26dd5ce
wallace <wallace_wolv@hotmail.com>
2024-01-05 14:50:29 -0300

    feat: (GET A PET) - Aula 22 Removendo Pet do sistema

--- 15_GETAPET/backend/controllers/PetController.js
+++ 15_GETAPET/backend/controllers/PetController.js
@@ -124,4 +124,34 @@ module.exports = class PetController {
 
     res.status(200).json({ pet });
   };
+
+  static async removePetById(req, res) {
+    const id = req.params.id;
+
+    if (!ObjectId.isValid(id)) {
+      res.status(422).json({ message: 'ID inválido!' });
+      return;
+    }
+
+    // check if pet exists
+    const pet = await Pet.findOne({ _id: id });
+
+    if (!pet) {
+      res.status(404).json({ message: 'Pet não encontrado!' });
+      return;
+    }
+
+    // checl if logged in user registered the pet
+    const token = getToken(req);
+    const user = await getUserByToken(token);
+
+    if (pet.user._id.toString() !== user._id.toString()) {
+      res.status(422).json({ message: 'Houve um problema em processar a sua solicitação, tente novamente mais tarde!' });
+      return;
+    }
+
+    await Pet.findByIdAndDelete(id);
+
+    res.status(200).json({ message: 'Pet removido com sucesso' });
+  };
 }
--- 15_GETAPET/backend/routes/PetRoutes.js
+++ 15_GETAPET/backend/routes/PetRoutes.js
@@ -16,5 +16,6 @@ router.get('/', PetController.getAll);
 router.get('/mypets', checkToken, PetController.getAllUserPets);
 router.get('/myadoptions', checkToken, PetController.getAllUserAdoptions);
 router.get('/:id', PetController.getPetById);
+router.delete('/:id', checkToken, PetController.removePetById);
 
 module.exports = router;
