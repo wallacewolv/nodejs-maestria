9821478fbef02ae526160dc307e9c1c661a91d69
wallace <wallace_wolv@hotmail.com>
2024-01-05 12:30:05 -0300

    feat: (GET A PET) - Aula 16 Salvando Pet no sistema

--- 15_GETAPET/backend/routes/PetRoutes.js
+++ 15_GETAPET/backend/routes/PetRoutes.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 
 const PetController = require('../controllers/PetController');
 
-router.post('/create', PetController.create);
+// middleware
+const checkToken = require('../helpers/check-token');
+
+router.post('/create', checkToken, PetController.create);
 
 module.exports = router;
