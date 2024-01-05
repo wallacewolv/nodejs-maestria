e863ef8ef3e0f5f0dfac640e65025da63cf5efcf
wallace <wallace_wolv@hotmail.com>
2024-01-05 20:47:02 -0300

    feat: (GET A PET) - Aula 36 Criando contexto de usuário

--- 15_GETAPET/frontend/src/App.js
+++ 15_GETAPET/frontend/src/App.js
@@ -11,24 +11,29 @@ import Home from './components/pages/Home';
 import Login from './components/pages/Auth/Login';
 import Register from './components/pages/Auth/Register';
 
+/* context */
+import { UserProvider } from './context/UserContext';
+
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Container>
-        <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Container>
-      <Footer />
+      <UserProvider>
+        <Navbar />
+        <Container>
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/register">
+              <Register />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Container>
+        <Footer />
+      </UserProvider>
     </Router>
   );
 }
