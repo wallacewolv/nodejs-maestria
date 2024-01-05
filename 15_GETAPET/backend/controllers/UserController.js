15fd2513438f913997157c03e7fe682887fefceb
wallace <wallace_wolv@hotmail.com>
2024-01-05 00:06:39 -0300

    feat: (GET A PET) - Aula 14 Upload de imagem do usuário

--- 15_GETAPET/backend/controllers/UserController.js
+++ 15_GETAPET/backend/controllers/UserController.js
@@ -159,7 +159,9 @@ module.exports = class UserController {
 
     const { name, email, phone, password, confirmpassword } = req.body;
 
-    let image = '';
+    if (req.file) {
+      user.image = req.file.filename;
+    }
 
     // validations
     if (!name) {
--- 15_GETAPET/backend/routes/UserRoutes.js
+++ 15_GETAPET/backend/routes/UserRoutes.js
@@ -4,11 +4,17 @@ const UserController = require('../controllers/UserController');
 
 // middleware
 const checkToken = require('../helpers/check-token');
+const { imageUpload } = require('../helpers/image-upload');
 
 router.post('/register', UserController.register);
 router.post('/login', UserController.login);
 router.get('/checkuser', UserController.checkUser);
 router.get('/:id', UserController.getById);
-router.patch('/edit/:id', checkToken, UserController.editUser);
+router.patch(
+  '/edit/:id',
+  checkToken,
+  imageUpload.single('image'),
+  UserController.editUser,
+);
 
 module.exports = router;
