8eeb9d30335b669eac534da7c68ecd4d72019e56
wallace <wallace_wolv@hotmail.com>
2024-01-06 13:01:00 -0300

    feat: (GET A PET) - Aula 41 Autenticando usuário cadastrado

--- 15_GETAPET/frontend/src/hooks/useAuth.js
+++ 15_GETAPET/frontend/src/hooks/useAuth.js
@@ -5,7 +5,18 @@ import { useHistory } from 'react-router-dom';
 import useFlashMessage from './useFlashMessage';
 
 export default function useAuth() {
+  const [authenticated, setAuthenticated] = useState(false);
   const { setFlashMessage } = useFlashMessage();
+  const history = useHistory();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`;
+      setAuthenticated(true);
+    }
+  }, []);
 
   async function register(user) {
     let msgText = 'Cadastro realizado com sucesso!';
@@ -16,7 +27,7 @@ export default function useAuth() {
         return response.data;
       });
 
-      console.log(data);
+      await authUser(data);
     } catch (error) {
       msgText = error.response.data.message;
       msgType = 'error';
@@ -25,5 +36,13 @@ export default function useAuth() {
     setFlashMessage(msgText, msgType);
   }
 
-  return { register };
+  async function authUser(data) {
+    setAuthenticated(true);
+
+    localStorage.setItem('token', JSON.stringify(data.token));
+
+    history.push('/');
+  }
+
+  return { authenticated, register };
 }
