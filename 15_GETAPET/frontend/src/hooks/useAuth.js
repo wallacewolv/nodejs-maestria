631e565e72639d8fdde816bc398eeb5d025ff23f
wallace <wallace_wolv@hotmail.com>
2024-01-06 12:47:48 -0300

    feat: (GET A PET) - Aula 40 Finalizando componente de flash message

--- 15_GETAPET/frontend/src/hooks/useAuth.js
+++ 15_GETAPET/frontend/src/hooks/useAuth.js
@@ -2,9 +2,15 @@ import api from '../utils/api';
 
 import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import useFlashMessage from './useFlashMessage';
 
 export default function useAuth() {
+  const { setFlashMessage } = useFlashMessage();
+
   async function register(user) {
+    let msgText = 'Cadastro realizado com sucesso!';
+    let msgType = 'success';
+
     try {
       const data = await api.post('/users/register', user).then((response) => {
         return response.data;
@@ -12,9 +18,11 @@ export default function useAuth() {
 
       console.log(data);
     } catch (error) {
-      // tratar erro
-      console.log(error);
+      msgText = error.response.data.message;
+      msgType = 'error';
     }
+
+    setFlashMessage(msgText, msgType);
   }
 
   return { register };
