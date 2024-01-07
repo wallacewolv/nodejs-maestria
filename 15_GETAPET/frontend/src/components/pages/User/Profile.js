6ba51576be07c2be78af03b5d79a32ef2cfc8ec7
wallace <wallace_wolv@hotmail.com>
2024-01-06 22:39:55 -0300

    feat: (GET A PET) - Aula 51 Criando componente de imagem

--- 15_GETAPET/frontend/src/components/pages/User/Profile.js
+++ 15_GETAPET/frontend/src/components/pages/User/Profile.js
@@ -8,6 +8,7 @@ import formStyles from '../../form/Form.module.css';
 import Input from '../../form/Input';
 
 import useFlashMessage from '../../../hooks/useFlashMessage';
+import RoundedImage from '../../layout/RoundedImage';
 
 function Profile() {
   const [user, setUser] = useState({});
@@ -91,7 +92,8 @@ function Profile() {
       <div className={styles.profile_header}>
         <h1>Perfil</h1>
         {(user.image || preview) && (
-          <img
+
+          <RoundedImage
             src={preview
               ? URL.createObjectURL(preview)
               : `${process.env.REACT_APP_API}/images/users/${user.image}`
